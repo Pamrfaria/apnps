4655a21a360f86d1d429dc2ccb2c8168e3c973be
Pamela Faria <pamelafaria.bg080@academico.ifsul.edu.br>
2021-03-31 18:52:48 -0300

    Criação a rota Layout

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 
-
 app.get("/",function(rec,res){
     res.send("Olá Pamela!")    
 })
 
 app.get("/mensagem",function(rec,res){
     res.send("Essa mensagem é automatica!")
+})
+
+app.get("/layout",function(rec,res){
+    res.send("Esse é o novo Layout!")
 })
 
 app.listen(3000,function(){
